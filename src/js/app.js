167d51e309a9c88884254c8b469d4812ab373545
Felix Ebert <felix.ebert@if-core.de>
2014-11-05 11:36:52 +0100

    talkie controls prototype, add angularjs

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
-(function (d3) {
+var talkies = [];
+(function (angular) {
+    var app = angular.module('app', ['angular-loading-bar']);
 
-})(d3);
+    app.controller('NavCtrl', function ($scope) {
+        $scope.talkie = null;
+        $scope.gotoTalkie = function(id) {
+            $scope.talkie.stop();
+            $scope.talkie = talkies[id];
+            $scope.talkie.start();
+        }
+    });
+})(angular);

1f1a8b3db15a140bd91347b4dc87a5961d478427
Felix Ebert <felix.ebert@if-core.de>
2014-11-19 11:29:41 +0100

    audio files map talkie prototype

--- src/js/targetSelection/targetSelectionDirective.js
+++ src/js/targetSelection/targetSelectionDirective.js
@@ -5,20 +5,25 @@
         leafletMap: null,
         init: function () {
             this.leafletMap = L.map('map', {
-                center: [50.665691, 10.451526],
-                zoom: 7,
+                center: [49.14, 9.22],
+                zoom: 12,
                 minZoom: 5,
-                maxZoom: 12
+                maxZoom: 20
             });
 
             this.addTileLayer();
         },
         addTileLayer: function () {
             var attribution = '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>';
-            L.tileLayer('https://{s}.tiles.mapbox.com/v3/pascalgabriel.k28h1mih/{z}/{x}/{y}.png', {
+            L.tileLayer('https://{s}.tiles.mapbox.com/v3/danielstahl.k90gp8cm/{z}/{x}/{y}.png', {
                 'maxZoom': 18,
                 'attribution': attribution
             }).addTo(this.leafletMap);
+        },
+        addImageLayer: function () {
+            var imageUrl = 'img/hn-1944.jpg',
+                imageBounds = [[49.11028727420634, 9.131011962890625], [49.24517269137362, 9.306106567382812]];
+            L.imageOverlay(imageUrl, imageBounds).addTo(this.leafletMap);
         }
     };
 
@@ -27,6 +32,18 @@
             restrict: 'A',
             link: function (scope, element, attrs, ctrl) {
                 map.init();
+
+                // TODO setTimeout is a temporary workaround
+                setTimeout(function () {
+                    var animate = Talkie.animate(element[0]);
+
+                    var timeline = Talkie.timeline("#ts-controls", {
+                        5: function () {
+                            map.addImageLayer();
+                        }
+                    });
+                    Talkie.ui.playButton("#ts-wrapper", timeline);
+                }, 500);
             }
         }
     });

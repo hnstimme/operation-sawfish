3ae9547662a7f6a06d676c381be922f9e455222c
Felix Ebert <felix.ebert@if-core.de>
2014-11-19 14:17:24 +0100

    prototype of section 2 - targtSelection add videogular dependencies to bower.json

--- src/js/targetSelection/targetSelectionDirective.js
+++ src/js/targetSelection/targetSelectionDirective.js
@@ -3,27 +3,35 @@
 
     var map = {
         leafletMap: null,
+        layers: {},
         init: function () {
             this.leafletMap = L.map('map', {
                 center: [49.14, 9.22],
-                zoom: 12,
+                zoom: 13,
                 minZoom: 5,
                 maxZoom: 20
             });
 
             this.addTileLayer();
+            this.addAreas();
         },
         addTileLayer: function () {
             var attribution = '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>';
+            // danielstahl.k91489gn
             L.tileLayer('https://{s}.tiles.mapbox.com/v3/danielstahl.k90gp8cm/{z}/{x}/{y}.png', {
                 'maxZoom': 18,
                 'attribution': attribution
             }).addTo(this.leafletMap);
         },
-        addImageLayer: function () {
-            var imageUrl = 'img/hn-1944.jpg',
-                imageBounds = [[49.11028727420634, 9.131011962890625], [49.24517269137362, 9.306106567382812]];
-            L.imageOverlay(imageUrl, imageBounds).addTo(this.leafletMap);
+        addAreas: function () {
+            var areas = ['areal1', 'areal2', 'areal3'];
+            var that = this;
+            areas.forEach(function (area) {
+                var imageUrl = 'img/' + area + '.svg',
+                    imageBounds = [[49.12107, 9.17307], [49.17003, 9.25135]];
+                that.layers[area] = L.imageOverlay(imageUrl, imageBounds).addTo(that.leafletMap);
+                that.layers[area].setOpacity(0);
+            })
         }
     };
 
@@ -35,13 +43,31 @@
 
                 // TODO setTimeout is a temporary workaround
                 setTimeout(function () {
-                    var animate = Talkie.animate(element[0]);
+                    var tl = TalkieLeaflet(map.leafletMap);
 
                     var timeline = Talkie.timeline("#ts-controls", {
-                        5: function () {
-                            map.addImageLayer();
+                        0.2: function () {
+                            map.layers['areal1'].setOpacity(1);
+                            this.setUndo(function () {
+                                map.layers['areal1'].setOpacity(0);
+                            })
+                        },
+                        10.9: tl.setView([49.14, 9.22], 14),
+                        11: function () {
+                            map.layers['areal2'].setOpacity(1);
+                            this.setUndo(function () {
+                                map.layers['areal2'].setOpacity(0);
+                            })
+                        },
+                        19.9: tl.setView([49.14, 9.22], 15),
+                        20: function () {
+                            map.layers['areal3'].setOpacity(1);
+                            this.setUndo(function () {
+                                map.layers['areal3'].setOpacity(0);
+                            })
                         }
                     });
+                    tl.undoViewChanges(timeline);
                     Talkie.ui.playButton("#ts-wrapper", timeline);
                 }, 500);
             }

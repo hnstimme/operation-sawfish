e3515432578cda435e1370b6693e8367e38c792d
Felix Ebert <felix.ebert@if-core.de>
2014-11-19 09:40:53 +0100

    navctrl remove unused function

--- src/js/NavCtrl.js
+++ src/js/NavCtrl.js
@@ -2,8 +2,5 @@
     'use strict';
 
     angular.module('app').controller('NavCtrl', function ($scope, $location) {
-        $scope.gotoTalkie = function (id) {
-            $location.href('/')
-        }
     });
 })(angular);

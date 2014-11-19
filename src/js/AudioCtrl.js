93e897f105582b0a05baa0fe97288f41863fd599
Daniel Nowak <daniel@nowak-it.com>
2014-11-19 12:52:46 +0100

    positioning of audiocontrols

--- src/js/AudioCtrl.js
+++ src/js/AudioCtrl.js
@@ -4,7 +4,7 @@
     angular.module('app').controller('AudioCtrl',
     function ($sce, $scope) {
 	$scope.config = {sources: [{src: $sce.trustAsResourceUrl("audio/1.mp3"), type: "audio/mpeg"}],
-	    theme: {url: "http://www.videogular.com/styles/themes/default/videogular.css"}
+	    theme: {url: "css/style.css"}
 	};
     });
 })(angular);

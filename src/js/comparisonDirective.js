9fde289fa0b1587dfa05d705508892dd9674cd23
Felix Ebert <felix.ebert@if-core.de>
2014-11-23 16:22:55 +0100

    comparison prototyping

--- src/js/comparisonDirective.js
+++ src/js/comparisonDirective.js
@@ -24,7 +24,9 @@
         },
         addCircleMarker: function (lat, lon) {
             var marker = L.circleMarker(L.latLng(lat, lon), {
-                fillColor: '#000000'
+                fillColor: '#FF0000',
+                strokeColor: '#FF0000',
+                color: '#FF0000'
             });
             map.leafletMap.addLayer(marker);
             return marker;
@@ -43,6 +45,71 @@
                     cities = geojson.features;
                 });
 
+                // chart
+                var margin = {top: 20, right: 20, bottom: 30, left: 50},
+                    width = 960 - margin.left - margin.right,
+                    height = 560 - margin.top - margin.bottom;
+
+                var x = d3.time.scale()
+                    .range([0, width]);
+
+                var y = d3.scale.linear()
+                    .range([height, 0]);
+
+                var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom");
+
+                var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .orient("left");
+
+                var line = d3.svg.line()
+                    .x(function (d) {
+                        return x(d.date);
+                    })
+                    .y(function (d) {
+                        return y(d.effectivity);
+                    });
+
+                var svg = d3.select(".comparison-chart")
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                var effectivityData;
+                d3.csv("data/effectivity.csv", function (error, data) {
+                    effectivityData = data;
+                    data.forEach(function (d) {
+                        d.date = moment(d.date, 'MM-YYYY');
+                        d.effectivity = +d.effectivity;
+                    });
+
+                    x.domain(d3.extent(data, function (d) {
+                        return d.date;
+                    }));
+                    y.domain(d3.extent(data, function (d) {
+                        return d.effectivity;
+                    }));
+
+                    svg.append("g")
+                        .attr("class", "x axis")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(xAxis);
+
+                    svg.append("g")
+                        .attr("class", "y axis")
+                        .call(yAxis)
+                        .append("text")
+                        .attr("transform", "rotate(-90)")
+                        .attr("y", 6)
+                        .attr("dy", ".71em")
+                        .style("text-anchor", "end")
+                        .text("Treffgenauigkeit der Nachtangriffe in Prozent");
+
+                    svg.append("path")
+                        .attr("class", "line")
+                });
+
                 // TODO setTimeout is a temporary workaround
                 setTimeout(function () {
                     var timelineDef = {};
@@ -58,12 +125,14 @@
                             });
                             var currentMonthsBetween = currentMoment.diff(startMoment, 'months');
                             var time = timePerMonth * currentMonthsBetween;
+
                             timelineDef[time] = function () {
                                 var markers = [];
                                 citiesToDisplay.forEach(function (city) {
                                     var marker = map.addCircleMarker(city.geometry.coordinates[1], city.geometry.coordinates[0]);
                                     markers.push(marker);
                                 });
+
                                 this.setUndo(function () {
                                     markers.forEach(function (marker) {
                                         map.leafletMap.removeLayer(marker);
@@ -72,6 +141,9 @@
                             };
                         })();
                     }
+                    timelineDef[0.1] = function () {
+                        d3.select('.line').classed('line-fill', true).datum(effectivityData).attr("d", line);
+                    };
 
                     Talkie.timeline("#audio-container audio", timelineDef);
                 }, 500);

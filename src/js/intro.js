d14a567f9467686e1164d535a8f036762b909935
Felix Ebert <felix.ebert@if-core.de>
2014-11-28 06:12:07 +0100

    #12 intro endscreen prototype poster image for intro chapter refactoring of intro directives

--- src/js/intro.js
+++ src/js/intro.js
@@ -11,24 +11,38 @@
             type: "video/ogg"
         }];
         $scope.videogularConfig.objectType = 'video';
+        $scope.videogularConfig.plugins = {
+            poster: "img/film_poster.jpg"
+        };
         $scope.play = function () {
             angular.element(document.getElementById('intro-video')).find('video')[0].play();
         }
     });
 
-    angular.module('app').directive("showVideo", function ($analytics) {
+    angular.module('app').directive("videogularApi", function () {
         return {
             restrict: "A",
             require: "^videogular",
             link: function (scope, elem, attrs, API) {
+                scope.videogularAPI = API;
+            }
+        }
+    });
+
+    angular.module('app').directive("trackVideoStart", function ($analytics) {
+        return {
+            restrict: "A",
+            require: "^videogular",
+            link: function (scope, elem, attrs, API) {
+                scope.videoHasStarted = false;
                 var unbindWatcher = scope.$watch(function () {
                     return API.currentState;
                 }, function (newVal) {
-                    if (newVal === 'play') {
-                        scope.showVideo = true;
+                    if (newVal === 'play' || newVal === 'pause') {
                         $analytics.eventTrack('playing', {
                             category: 'Heilbronn brennt'
                         });
+                        scope.videoHasStarted = true;
                         unbindWatcher();
                     }
                 });

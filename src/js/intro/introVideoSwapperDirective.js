95f6213660cc67e182a9ed0005401f9f369c45dc
Felix Ebert <felix.ebert@if-core.de>
2014-12-02 02:53:25 +0100

    introVideoSwapper: use a more reliable method to autostart the video

--- src/js/intro/introVideoSwapperDirective.js
+++ src/js/intro/introVideoSwapperDirective.js
@@ -5,18 +5,42 @@
             restrict: "A",
             require: "^videogular",
             link: function (scope, elem, attrs, API) {
-                scope.fullVideoLoaded = false;
+                scope.fullVideoConfigured = false;
                 scope.showFullVideo = function () {
                     API.clearMedia();
+                    scope.fullVideoIsLoading = true;
+                    scope.fullVideoConfigured = true;
                     scope.videogularConfig.sources = [{
                         src: "mov/film_komplett.mp4",
                         type: "video/mp4"
                     }];
-                    $timeout(function () {
-                        API.play();
-                        scope.fullVideoLoaded = true;
-                    }, 500);
-                }
+
+                    var tryToPlayPromise;
+                    var tryToPlay = function () {
+                        tryToPlayPromise = $timeout(function () {
+                            if (scope.fullVideoIsLoading) {
+                                API.play();
+                                tryToPlay();
+                            }
+                        }, 100, false);
+                    };
+                    tryToPlay();
+
+                    var unbindWatcher = scope.$watch(function () {
+                        return API.currentState;
+                    }, function (newVal) {
+                        if (newVal === 'play') {
+                            scope.fullVideoIsLoading = false;
+                            unbindWatcher();
+                            $timeout.cancel(tryToPlayPromise);
+                        }
+                    });
+
+                    scope.$on('$destroy', function () {
+                        unbindWatcher();
+                        $timeout.cancel(tryToPlayPromise);
+                    });
+                };
             }
         }
     });

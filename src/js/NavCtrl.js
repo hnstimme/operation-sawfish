dd9495251fb55285fe2aa63bff73e47747bb9bba
Felix Ebert <felix.ebert@if-core.de>
2014-12-04 01:02:52 +0100

    add share function

--- src/js/NavCtrl.js
+++ src/js/NavCtrl.js
@@ -2,8 +2,5 @@
     'use strict';
 
     angular.module('app').controller('NavCtrl', function ($scope, $location) {
-        $scope.showInfoScreen = function () {
-            $scope.modals.infoScreen = !$scope.modals.infoScreen;
-        }
     });
 })(angular);
--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,13 @@
         'com.2fdevs.videogular.plugins.overlayplay',
         'com.2fdevs.videogular.plugins.poster',
         'angulartics',
-        'angulartics.google.analytics'
+        'angulartics.google.analytics',
+        'djds4rce.angular-socialshare'
     ]);
+
+    angular.module('app').run(function ($FB) {
+        $FB.init('1588324454735392');
+    });
 })(angular);
 
 

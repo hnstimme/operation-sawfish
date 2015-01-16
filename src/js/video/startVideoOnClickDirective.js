c734d46ac33e5a1b7b1e125ec7ea6888bb1fbade
Felix Ebert <felix.ebert@if-core.de>
2015-01-16 16:55:52 +0100

    deactivate autoplay on iOS

--- src/js/video/startVideoOnClickDirective.js
+++ src/js/video/startVideoOnClickDirective.js
@@ -6,8 +6,9 @@
             require: "^videogular",
             link: function (scope, elem, attrs, API) {
                 var listener;
+                var iOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
                 var activateListener = function () {
-                    if (!listener) {
+                    if (!listener && !iOS) {
                         listener = $rootScope.$on('$routeChangeSuccess', function () {
                             listener();
                             $timeout(function () {
